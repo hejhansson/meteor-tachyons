fbc74fe9d9247a240d20ff61f389cb18c5be751d
Alexander Hansson <alexander.hansson@bulldozer.se>
2016-01-22 10:51:44 +0100

    Fix: correct css file

--- package.js
+++ package.js
@@ -1,12 +1,12 @@
 Package.describe({
   name: 'hnssn:tachyons',
   version: '0.0.1',
-  summary: 'Tachyons packages for Meteor',
+  summary: 'Tachyons packaged for Meteor',
   git: 'https://github.com/hejhansson/meteor-tachyons.git',
   documentation: 'README.md'
 });
 
 Package.onUse(function(api) {
   api.versionsFrom('1.0');
-  api.addFiles('lib/css/basscss.css', 'client');
+  api.addFiles('lib/css/tachyons.min.css', 'client');
 });
