487bbc249ae1e20004965da38295a8565efdda50
Alexander Hansson <hejhansson@users.noreply.github.com>
2017-01-04 11:45:31 +0100

    Update package.js

--- package.js
+++ package.js
@@ -1,12 +1,12 @@
 Package.describe({
   name: 'hasse:tachyons',
-  version: '1.0',
+  version: '1.1',
   summary: 'Tachyons packaged for Meteor',
   git: 'https://github.com/hejhansson/meteor-tachyons.git',
   documentation: 'README.md'
 });
 
 Package.onUse(function(api) {
-  api.versionsFrom('1.0');
+  api.versionsFrom('1.1');
   api.addFiles('lib/css/tachyons.min.css', 'client');
 });
